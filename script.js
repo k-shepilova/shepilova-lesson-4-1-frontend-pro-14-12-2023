bb7327bf44ffb694276a31b8918c092201933937
Kateryna Shepilova <152919831+k-shepilova@users.noreply.github.com>
2023-12-14 20:53:46 +0200

    added calculation

---
+++ script.js
@@ -0,0 +1,26 @@
+const firstNum = +prompt('Please enter the first num');
+const secondNum = +prompt('Please enter the second num');
+const rule = prompt('Please enter the rule');
+
+const calculationRules = {
+  sum: '+',
+  dif: '-',
+  mult: '*',
+  div: '/'
+}
+
+function getCalculation(firstNum, secondNum, rule) {
+  if (rule === calculationRules.sum) {
+    return firstNum + secondNum;
+  } else if (rule === calculationRules.dif) {
+    return firstNum - secondNum;
+  } else if (rule === calculationRules.mult) {
+    return firstNum * secondNum;
+  } else if (rule === calculationRules.div) {
+    return firstNum / secondNum;
+  } else {
+    return 'Please enter correct data';
+  }
+}
+
+console.log(getCalculation(2, 5, rule));
